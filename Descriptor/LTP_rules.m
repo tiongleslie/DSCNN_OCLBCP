bf598fc1655a68dd6363c922d49db21cde56c5f8
tiongleslie <28769811+tiongleslie@users.noreply.github.com>
2020-03-28 13:04:11 +0900

    Delete LTP_rules.m

--- Descriptor/LTP_rules.m
+++
@@ -1,9 +0,0 @@
-function pixel = LTP_rules(p, c, thres)
-    if (p >= (c - thres)) && (p <= (c + thres))
-        pixel = 0;
-    elseif (p < (c - thres))
-        pixel = -1;
-    elseif (p > (c + thres))
-        pixel = 1;
-    end
-end
